8ed356e4c31b004e45ea4150c1ab76a3c7388fa0
codecsp <mr.soham143@gmail.com>
2020-07-13 13:01:34 +0530

    added files

---
+++ main.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import datetime
+import smtplib
+
+GmailID = ''
+gmailPass = ''
+
+def sendEmail(to,sub,msg):
+	print("msg sent to :"+ to)
+	s=smtplib.SMTP('smtp.gmail.com',587)
+	s.starttls()
+	s.login(GmailID,gmailPass)
+	s.sendmail(GmailID,to,f"Subject: {sub}\n\n{msg}")
+	s.quit()
+
+if __name__ == '__main__':
+	df = pd.read_excel("data.xlsx")
+	#print(df)
+	today = datetime.datetime.now().strftime("%d-%m") #day and month only
+	yearnow = datetime.datetime.now().strftime("%Y")
+	
+	#print(type(today))
+	writeind = []
+
+	for index, item in df.iterrows():
+		bday = item['Birthday'].strftime("%d-%m")
+		to= item['Email']
+		msg = item['Dialogue']
+		sub= "HappY Birthday "+ item['Name']
+		#print(index, item['Birthday'])
+		#print(bday)
+		if(today==bday) and yearnow not in str(item['Year']):
+			sendEmail(to,sub,msg)
+			writeind.append(index)
+
+
+	#print(writeind)
+	for i in writeind:
+		yr = str(df.loc[i,'Year'])
+		df.loc[i,'Year'] = yr+","+yearnow;
+		#print(df.loc[i,'Year'])
+
+	#print(df)
+	df.to_excel('data.xlsx',index=False)
